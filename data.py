564d18f2448561f033f49d3c03e21fad63f7611f
Chaplin <ann794@yandex.ru>
2026-05-04 21:17:24 +0300

    add test for burger

--- data.py
+++ data.py
@@ -4,3 +4,6 @@ BUN_PRICE = 100
 
 INGREDIENT_NAME = 'cheese'
 INGREDIENT_PRICE = 100
+
+ONE_INGREDIENT = [('filling', 'chicken', 100)]
+TWO_INGREDIENTS = [('filling', 'chicken', 100), ('sauce', 'ketchup', 50)]
---
+++ tests/test_burger.py
@@ -0,0 +1,144 @@
+import data as d
+import pytest
+
+from unittest.mock import Mock
+
+from praktikum.burger import Burger
+from praktikum.ingredient import Ingredient
+from praktikum.bun import Bun
+
+class TestBurger():
+
+    def test_burger_empty_buns(self):
+        burger = Burger()
+
+        assert burger.bun is None
+
+
+    def test_burger_empty_ingredients(self):
+        burger = Burger()
+
+        assert burger.ingredients == []
+
+
+    def test_burger_set_bun(self):
+        burger = Burger()
+        bun_mock = Mock()
+
+        burger.set_buns(bun_mock)
+        assert burger.bun == bun_mock
+
+
+    def test_burger_add_ingredient(self):
+        burger = Burger()
+        ingredient_mock = Mock()
+
+        burger.add_ingredient(ingredient_mock)
+        assert burger.ingredients[0] == ingredient_mock
+
+    def test_burger_remove_ingredient(self):
+        burger = Burger()
+        ingredient_mock = Mock()
+
+        burger.add_ingredient(ingredient_mock)
+        burger.remove_ingredient(0)
+        assert burger.ingredients == []
+
+    def test_burger_move_ingredient(self):
+        burger = Burger()
+        ingredient_mock_1 = Mock()
+        ingredient_mock_2 = Mock()
+
+        burger.add_ingredient(ingredient_mock_1)
+        burger.add_ingredient(ingredient_mock_2)
+
+        burger.move_ingredient(0, 1)
+        assert burger.ingredients[0] == ingredient_mock_2 and burger.ingredients[-1] == ingredient_mock_1
+
+
+    @pytest.mark.parametrize(
+        "ingredient_list, price",
+        [(d.ONE_INGREDIENT, 120), (d.TWO_INGREDIENTS, 170), ("", 20)],
+        ids=["one_ing", "two_ing", "empty_ing"],
+    )
+    def test_burger_get_price(self, ingredient_list, price):
+        burger = Burger()
+        bun = Bun('lavash', 10)
+
+        burger.set_buns(bun)   
+
+        for i in ingredient_list:
+            ingredient = Ingredient(*i)
+            burger.add_ingredient(ingredient)
+
+        assert burger.get_price() == price
+
+
+    @pytest.mark.parametrize(
+        "ingredient_list, price",
+        [(d.ONE_INGREDIENT, 120), (d.TWO_INGREDIENTS, 170), ("", 20)],
+        ids=["one_ing", "two_ing", "empty_ing"],
+    )
+    def test_burger_get_receipt(self, ingredient_list, price):
+        burger = Burger()
+        bun = Bun('lavash', 10)
+
+        burger.set_buns(bun)   
+         
+        for i in ingredient_list:
+            ingredient = Ingredient(*i)
+            burger.add_ingredient(ingredient)
+
+        receipt = burger.get_receipt()
+
+        assert bun.get_name(), burger.get_price() in receipt
+
+
+
+
+
+
+
+
+# # class Burger:
+#     """
+#     Модель бургера.
+#     Бургер состоит из булочек и ингредиентов (начинка или соус).
+#     Ингредиенты можно перемещать и удалять.
+#     Можно распечать чек с информацией о бургере.
+#     """
+
+#     def __init__(self):
+#         self.bun = None
+#         self.ingredients: List[Ingredient] = []
+
+#     def set_buns(self, bun: Bun):
+#         self.bun = bun
+
+#     def add_ingredient(self, ingredient: Ingredient):
+#         self.ingredients.append(ingredient)
+
+#     def remove_ingredient(self, index: int):
+#         del self.ingredients[index]
+
+#     def move_ingredient(self, index: int, new_index: int):
+#         self.ingredients.insert(new_index, self.ingredients.pop(index))
+
+#     def get_price(self) -> float:
+#         price = self.bun.get_price() * 2
+
+#         for ingredient in self.ingredients:
+#             price += ingredient.get_price()
+
+#         return price
+
+#     def get_receipt(self) -> str:
+#         receipt: List[str] = [f'(==== {self.bun.get_name()} ====)']
+
+#         for ingredient in self.ingredients:
+#             receipt.append(f'= {str(ingredient.get_type()).lower()} {ingredient.get_name()} =')
+
+#         receipt.append(f'(==== {self.bun.get_name()} ====)\n')
+#         receipt.append(f'Price: {self.get_price()}')
+
+#         return '\n'.join(receipt)
